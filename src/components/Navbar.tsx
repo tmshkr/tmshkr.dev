4a30de9316bb9ba546e0dbf41dc37f1d663b3856
Tim Shaker <tim@tmshkr.com>
2022-08-11 16:21:08 -0700

    dark mode toggle

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,49 +1,32 @@
 import * as React from "react"
 import { Disclosure, Menu, Transition } from "@headlessui/react"
-import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline"
+import { MoonIcon, MenuIcon, XIcon } from "@heroicons/react/outline"
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ")
-}
+declare var window: any
+
+const activeItemClasses =
+  "border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+const inactiveItemClasses =
+  "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="bg-white shadow">
+    <Disclosure as="nav" className="bg-white dark:bg-black">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between h-16">
               <div className="flex">
-                <div className="flex-shrink-0 flex items-center">
-                  <img
-                    className="block lg:hidden h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                    alt="Workflow"
-                  />
-                  <img
-                    className="hidden lg:block h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
-                    alt="Workflow"
-                  />
-                </div>
+                <div className="flex-shrink-0 flex items-center">tmshkr</div>
                 <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
                   {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
-                  <a
-                    href="#"
-                    className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                  >
+                  <a href="#" className={activeItemClasses}>
                     About
                   </a>
-                  <a
-                    href="#"
-                    className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                  >
+                  <a href="#" className={inactiveItemClasses}>
                     Blog
                   </a>
-                  <a
-                    href="#"
-                    className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                  >
+                  <a href="#" className={inactiveItemClasses}>
                     Projects
                   </a>
                 </div>
@@ -53,7 +36,15 @@ export default function Navbar() {
                   type="button"
                   className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  <MoonIcon
+                    className="h-6 w-6"
+                    aria-hidden="true"
+                    onClick={() =>
+                      window.__setPreferredTheme(
+                        window.__theme === "dark" ? "light" : "dark"
+                      )
+                    }
+                  />
                 </button>
               </div>
               <div className="-mr-2 flex items-center sm:hidden">
