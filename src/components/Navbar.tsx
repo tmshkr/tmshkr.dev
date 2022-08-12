59a2ed76e618e1a6183b1f9f6aa311f3a8b722e3
Tim Shaker <tim@tmshkr.com>
2022-08-11 19:32:21 -0700

    toggle dark mode mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,10 +13,15 @@ const inactiveItemClasses =
 export default function Navbar() {
   return (
     <Disclosure as="nav">
-      {({ open }) => (
-        <>
-          <div className="max-w-2xl mx-auto px-4 md:px-0">
-            <div className="flex justify-between h-16">
+      {({ open, close }) => {
+        if (open) {
+          window.onclick = close
+        } else {
+          window.onclick = null
+        }
+        return (
+          <>
+            <div className="max-w-2xl mx-auto px-4 md:px-0 flex justify-between h-16">
               <div className="flex items-center">
                 <Link to="/" className="text-gray-500 no-underline">
                   tmshkr
@@ -53,46 +58,57 @@ export default function Navbar() {
               </div>
               <div className="-mr-2 flex items-center sm:hidden">
                 {/* Mobile menu button */}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <Disclosure.Button
+                  className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                  onClick={e => e.stopPropagation()}
+                >
                   <span className="sr-only">Open main menu</span>
                   {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    <MoonIcon
+                      className="h-6 w-6 dark:fill-current"
+                      aria-hidden="true"
+                      onClick={() =>
+                        window.__setPreferredTheme(
+                          window.__theme === "dark" ? "light" : "dark"
+                        )
+                      }
+                    />
                   ) : (
                     <MenuIcon className="block h-6 w-6" aria-hidden="true" />
                   )}
                 </Disclosure.Button>
               </div>
             </div>
-          </div>
 
-          <Disclosure.Panel className="sm:hidden">
-            <div className="pt-2 pb-3 space-y-1">
-              {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-              >
-                About
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-              >
-                Blog
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-              >
-                Projects
-              </Disclosure.Button>
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
+            <Disclosure.Panel className="sm:hidden">
+              <div className="pt-2 pb-3 space-y-1">
+                {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                >
+                  About
+                </Disclosure.Button>
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                >
+                  Blog
+                </Disclosure.Button>
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                >
+                  Projects
+                </Disclosure.Button>
+              </div>
+            </Disclosure.Panel>
+          </>
+        )
+      }}
     </Disclosure>
   )
 }
