7f686eb9b96a5937ac81d7db0b99fab662647017
Tim Shaker <tim@tmshkr.com>
2022-08-15 16:29:58 -0700

    add code theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,4 @@
-// custom typefaces
-import "typeface-montserrat"
 import "typeface-merriweather"
-// normalize CSS across browsers
-// import "./src/normalize.css"
-// custom CSS styles
-// import "./src/style.css"
-
-// Highlighting for code blocks
-import "prismjs/themes/prism.css"
-
-import "./src/tailwind.css"
 import "./src/global.css"
+import "./src/tailwind.css"
+import "./src/code.scss"
