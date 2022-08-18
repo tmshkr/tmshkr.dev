19999c79b40c830eab1460e3f2a1deef38af20d4
Tim Shaker <tim@tmshkr.com>
2022-08-17 20:26:18 -0700

    add body-scroll-lock

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -11,6 +11,8 @@ import algoliasearch from "algoliasearch/lite"
 import { InstantSearch, SearchBox, Hits } from "react-instantsearch-hooks-web"
 import { SearchIcon } from "@heroicons/react/outline"
 
+const { disableBodyScroll, enableBodyScroll } = require("body-scroll-lock")
+
 import "./Search.scss"
 
 const searchClient = algoliasearch(
@@ -29,12 +31,28 @@ function Hit({ hit }) {
 
 export function Search() {
   const [isOpen, setIsOpen] = useState(false)
+  const scrollDiv = useRef(null)
+
+  const closeModal = () => {
+    enableBodyScroll(scrollDiv.current)
+    setIsOpen(false)
+  }
+  const openModal = () => {
+    setIsOpen(true)
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      disableBodyScroll(scrollDiv.current)
+    }
+  }, [isOpen])
+
   return (
     <InstantSearch searchClient={searchClient} indexName="pages">
       <div className="flex items-center ml-auto">
         <button
           className="dark:bg-slate-600 py-2 px-4 rounded-md"
-          onFocus={() => setIsOpen(true)}
+          onFocus={openModal}
         >
           <SearchIcon className="w-5 inline mr-2 -mt-1" />
           Search
@@ -43,12 +61,13 @@ export function Search() {
       {isOpen &&
         createPortal(
           <div
-            onClick={() => setIsOpen(false)}
+            onClick={closeModal}
             className="bg-white/60 dark:bg-slate-500/60 backdrop-blur-sm dark:text-white absolute top-0 right-0 left-0 bottom-0"
           >
             <div
+              ref={scrollDiv}
               onClick={e => e.stopPropagation()}
-              className="bg-white dark:bg-slate-800 max-w-lg mt-24 mx-auto p-4 rounded-lg shadow-lg"
+              className="bg-white dark:bg-slate-800 max-w-lg max-h-[100%] sm:max-h-[80%] min-h-[33%] overflow-scroll bg-scroll mt-0 sm:mt-24 mx-auto p-4 rounded-lg shadow-lg"
             >
               <SearchBox />
               <Hits hitComponent={Hit} />
