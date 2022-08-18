6135bc6e15b076eb7b8ac351cbb2b5883f7285ef
Tim Shaker <tim@tmshkr.com>
2022-08-18 10:28:23 -0700

    modal layout

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { createPortal } from "react-dom"
 import algoliasearch from "algoliasearch/lite"
 import { InstantSearch, SearchBox, Hits } from "react-instantsearch-hooks-web"
@@ -13,15 +13,6 @@ const searchClient = algoliasearch(
   "QOE9A9XPBA",
   "51ef76747e385c0819fc9a496322b785"
 )
-function Hit({ hit }) {
-  return (
-    <article>
-      <h1>{hit.frontmatter.title}</h1>
-      <p>{hit.frontmatter.date}</p>
-      <p>{hit.excerpt}</p>
-    </article>
-  )
-}
 
 export function Search() {
   const [isOpen, setIsOpen] = useState(false)
@@ -33,15 +24,19 @@ export function Search() {
     setIsOpen(true)
   }
 
+  useEffect(() => {
+    Mousetrap.bind(["command+k", "ctrl+k"], openModal)
+  }, [])
+
   return (
     <InstantSearch searchClient={searchClient} indexName="pages">
       <div className="flex items-center ml-auto">
         <button
-          className="dark:bg-slate-600 py-2 px-4 rounded-md"
+          className="dark:bg-slate-600 p-2 rounded-md"
           onClick={openModal}
         >
           <SearchIcon className="w-5 inline mr-2 -mt-1" />
-          Search
+          Search <span className="bg-slate-500 rounded-md ml-2 p-1">⌘K</span>
         </button>
       </div>
       {isOpen &&
@@ -50,30 +45,30 @@ export function Search() {
   )
 }
 
-class SearchModal extends React.Component<any, any> {
-  private scrollDiv = React.createRef<HTMLDivElement>()
+class SearchModal extends React.PureComponent<any, any> {
+  private modal = React.createRef<HTMLDivElement>()
 
   componentDidMount() {
     Mousetrap.bind("esc", this.props.closeModal)
-    disableBodyScroll(this.scrollDiv.current)
+    disableBodyScroll(this.modal.current)
   }
   componentWillUnmount() {
     Mousetrap.unbind("esc")
-    enableBodyScroll(this.scrollDiv.current)
+    enableBodyScroll(this.modal.current)
   }
 
   render() {
     return (
       <div
         onClick={this.props.closeModal}
-        className="bg-white/60 dark:bg-slate-500/60 backdrop-blur-sm dark:text-white fixed top-0 right-0 left-0 bottom-0"
+        className="bg-white/60 dark:bg-slate-500/60 dark:text-white fixed top-0 right-0 left-0 bottom-0"
       >
         <div
-          ref={this.scrollDiv}
+          ref={this.modal}
           onClick={e => e.stopPropagation()}
-          className="bg-white dark:bg-slate-800 w-full sm:max-w-lg max-h-[100%] sm:max-h-[80%] min-h-[33%] overflow-scroll mt-0 sm:mt-24 mx-auto p-4 rounded-b-lg sm:rounded-lg shadow-lg"
+          className="flex flex-col bg-white dark:bg-slate-800 w-full sm:max-w-lg max-h-full sm:max-h-[80%] mt-0 sm:mt-24 mx-auto sm:rounded-lg shadow-lg"
         >
-          <div className="flex">
+          <div className="flex p-4">
             <button
               onClick={this.props.closeModal}
               className="py-1 px-2 mr-2 bg-slate-500 rounded-[3px]"
@@ -82,9 +77,19 @@ class SearchModal extends React.Component<any, any> {
             </button>
             <SearchBox className="w-full" />
           </div>
-          <Hits hitComponent={Hit} />
+          <Hits hitComponent={Hit} className="overflow-scroll px-4" />
         </div>
       </div>
     )
   }
+}
+
+function Hit({ hit }) {
+  return (
+    <article>
+      <h1>{hit.frontmatter.title}</h1>
+      <p>{hit.frontmatter.date}</p>
+      <p>{hit.excerpt}</p>
+    </article>
+  )
 }
