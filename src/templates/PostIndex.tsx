4b8303f812ebec78b915e7a099dc07cd42d18d3f
Tim Shaker <tim@tmshkr.com>
2022-08-19 18:08:23 -0700

    update PostIndex page title

--- src/templates/PostIndex.tsx
+++ src/templates/PostIndex.tsx
@@ -82,7 +82,10 @@ export default PostIndex
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="All posts" />
+export const Head = ({ location }) => {
+  const pathRoot = getPathRoot(location.pathname)
+  return <Seo title={pathRoot} />
+}
 
 export const pageQuery = graphql`
   query PaginatedIndex($skip: Int!, $limit: Int!, $glob: String!) {
